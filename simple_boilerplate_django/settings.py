d75ee28319c49e6b63151005576143441c838ed2
Kukuh <kukuhsain@gmail.com>
2017-02-19 13:35:34 +0700

    Set up Posgresql database configuration

--- simple_boilerplate_django/settings.py
+++ simple_boilerplate_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'uxwjp8%^n+yh^xhipmch8ofhu!))l(^z^j70_6y=7x(b@6n$q='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['simple-boilerplate.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -73,13 +74,16 @@ WSGI_APPLICATION = 'simple_boilerplate_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=500),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
