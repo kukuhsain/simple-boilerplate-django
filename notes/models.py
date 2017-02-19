575583e831cb7dd29292c9fbd044638755da867d
Kukuh <kukuhsain@gmail.com>
2017-02-19 16:59:50 +0700

    Add Note model into notes app

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('created',)
